7225e8c10b5011b4dd88030e678a761574b9f788
Dmitry Pyzhov <fiche40@gmail.com>
2022-05-30 20:15:58 +0400

    Initial Commit

---
+++ GIPHYApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GIPHYApp
+//
+//  Created by Dmitry Pyzhov on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
